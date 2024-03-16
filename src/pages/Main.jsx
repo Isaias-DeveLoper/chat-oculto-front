210cd859ed4e2d498d8e5a41a504c257cd6085df
Isaias Vasconcelos <isaiaskeyboards@gmail.com>
2024-03-15 23:49:58 -0300

    -

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -13,7 +13,7 @@ export default function Main() {
 
         let nomeCriptografado = sha256(usuario).substring(20);
 
-        localStorage.setItem('usuario',nomeCriptografado);
+        localStorage.setItem('usuario', nomeCriptografado);
 
         setUsuario('');
 
@@ -24,28 +24,32 @@ export default function Main() {
             <div className="d-flex justify-content-center"
                 style={{
                     position: 'relative',
-                    top: '300px'
+                    top: '150px'
                 }}
 
             >
-                <form onSubmit={Entrar} className="d-flex flex-column justify-content-center gap-2 w-25">
-                    
+                <form onSubmit={Entrar} className="d-flex flex-column justify-content-center gap-3 w-25">
+
+                    <h3 className="d-flex justify-content-center align-items-center text-white">
+                        <b>Envie mensagens para qualquer pessoa conectada sem ser idêntificado 😶❌</b>
+                    </h3>
+
                     <input
                         type="text"
                         style={{
-                            backgroundColor:'#0b141a',
-                            color:'white',
-                            borderColor:'#005c4b'
+                            backgroundColor: '#0b141a',
+                            color: 'white',
+                            borderColor: '#005c4b'
                         }}
                         className="form-control"
-                        placeholder="Digite seu nome secreto"
+                        placeholder="Digite seu nome secreto... Ex: girafa"
                         required
                         value={usuario}
                         onChange={e => setUsuario(e.target.value)}
                     />
 
-                    <button style={{ backgroundColor:'#005c4b',color:'white' }} type="submit" className="btn">
-                        ENTRAR
+                    <button style={{ backgroundColor: '#005c4b', color: 'white' }} type="submit" className="btn">
+                        <b>ENTRAR</b>
                     </button>
 
                 </form>
